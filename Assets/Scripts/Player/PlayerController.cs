4254835228ecea150ce3fbde48a8316ba75590c1
diegoesedin <diego.e8@hotmail.com>
2021-01-30 10:55:53 -0300

    Add first player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private Vector3 targetPosition;
+        private Vector3 direction;
+
+        private Rigidbody2D rigidbody;
+
+        void Start()
+        {
+            rigidbody = this.GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+                Move();
+
+            if (Input.GetMouseButtonUp(0))
+                direction = Vector2.zero;
+        }
+
+        void FixedUpdate()
+        {
+            rigidbody.MovePosition(transform.position + direction * speed * Time.deltaTime);
+        }
+
+        void SetTargetPosition()
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = transform.position.z;
+        }
+
+        void Move()
+        {
+            SetTargetPosition();
+            direction = (targetPosition - transform.position).normalized;
+        }
+    }
+}
