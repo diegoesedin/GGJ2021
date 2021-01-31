a25f012abf1e10876e422e743929166294e33ca7
diegoesedin <diego.e8@hotmail.com>
2021-01-30 23:15:40 -0300

    Add dotween and animation for final selection

---
+++ Assets/Scripts/UI/PaperSelection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GGJ.UI
+{
+    public class PaperSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            this.transform.DOScale(1.2f, 0.5f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            this.transform.DOScale(1f, 0.5f);
+        }
+    }
+}
